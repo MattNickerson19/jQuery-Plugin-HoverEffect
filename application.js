bd45d0d6979a925c6542d0c75ac25e949f6a1855
MattNickerson19 <matt.nickerson96@gmail.com>
2021-11-16 20:48:39 -0330

    "Add functionality"

---
+++ application.js
@@ -0,0 +1,7 @@
+$("#imageGallery").imageHover({
+    opacity: 1,
+            height: '160px',
+            width: '190px',
+            time: 1000
+    
+});
--- jquery.hoverEffect.js
+++ jquery.hoverEffect.js
@@ -1,4 +1,49 @@
 (function($){
+    $.fn.imageHover = function(options){
+
+        var settings = $.extend({
+            opacity: 1,
+            height: '160px',
+            width: '190px',
+            time: 1000
+            
+
+        }, options)
+
+        return this.each(function(){
+
+            
+
+            enter()
+            leave()
+            
+
+
+            function enter(event){
+
+                $("img").mouseenter(function(){
+                    $(this).css({"opacity": "0.7"}).animate({"height": settings.height, "width": settings.width}, settings.time);
+                })
+
+            }
+
+            
+
+            
+            function leave(event){
+
+                $("img").mouseleave(function(){
+                    $(this).css({"opacity": settings.opacity}).animate({"height": "140px", "width": "170px"}, 1000);
+                })
+
+            }
+            
+
+
+        })
+
+        
+    }
 
     
 }(jQuery));
