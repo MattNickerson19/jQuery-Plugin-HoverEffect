f5e4343e3c0ab04c4fedf6f8b2ccaba53ccf45f6
MattNickerson19 <matt.nickerson96@gmail.com>
2021-11-25 19:33:54 -0330

    "Updated effect"

--- jquery.hoverEffect.js
+++ jquery.hoverEffect.js
@@ -5,44 +5,34 @@
             opacity: 1,
             height: '160px',
             width: '190px',
-            time: 1000
+            time: 1000,
             
-
         }, options)
 
         return this.each(function(){
-
-            
-
             enter()
             leave()
             
-
-
             function enter(event){
 
                 $("img").mouseenter(function(){
-                    $(this).css({"opacity": "0.7"}).animate({"height": settings.height, "width": settings.width}, settings.time);
+                    $(this).css({"opacity": "0.6"}).animate({"height": settings.height, "width": settings.width,}, settings.time);
+                    var title = $(this).attr('alt');
+                    $(this).after('<p></p>');
+                    $(this).next().text(title).animate({"right": "0px"}, settings.time);
                 })
 
             }
 
-            
-
-            
             function leave(event){
-
                 $("img").mouseleave(function(){
                     $(this).css({"opacity": settings.opacity}).animate({"height": "140px", "width": "170px"}, 1000);
-                })
-
+                    $(this).next().animate({"left": "-15px", "opacity": "0"}, settings.time, function(){
+                        $(this).next().remove();
+                    });
+                });
             }
-            
-
-
-        })
-
-        
+        }); 
     }
 
     
